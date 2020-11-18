c1ba4a54857eb9341ab098f60cd657ab493d3a6b
DiptoChakrabarty <diptochuck123@gmail.com>
2020-11-18 09:44:54 +0530

    class objects

--- receive.py
+++ receive.py
@@ -5,16 +5,16 @@ def main():
     connection =  pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
     channel = connection.channel()
 
-    channel.queue_declare(queue="sample")
+    channel.queue_declare(queue="sample-queue")
 
-    def callback(ch,method,properties,body):
-        print("Received message",body)
-
-    channel.basic_consume(queue="sample",
+    channel.basic_consume(queue="sample-queue",
     on_message_callback=callback,auto_ack=True)
 
     print("Waiting for the messages\n")
     channel.start_consuming()
+
+def callback(ch,method,properties,body):
+        print("Received message",body)
 
 
 
--- send.py
+++ send.py
@@ -1,12 +1,23 @@
 import pika 
 
-connection =  pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
-channel = connection.channel()
 
-channel.queue_declare(queue="sample")
-channel.basic_publish(exchange='',
-            routing_key="sample",
-            body="sample body")
-print("Sample Message Sent to rabbitmq")
-connection.close()
-print("Connection closed")
+
+
+class RabbitMq():
+    def __init__(self,queue,host="localhost"):
+        self.connection = connection =  pika.BlockingConnection(pika.ConnectionParameters(host=host))
+        self.channel = self.connection.channel()
+        self.queue=queue
+        self.channel.queue_declare(queue=self.queue)
+    
+    def publish(self,message):
+        self.channel.basic_publish(exchange='',
+            routing_key=self.queue,
+            body=str(message))
+        print("Sample Message Sent to rabbitmq")
+        self.connection.close()
+        print("Connection closed")
+    
+if __name__=="__main__":
+    server = RabbitMq(queue="sample-queue")
+    server.publish("This is from the sender message")

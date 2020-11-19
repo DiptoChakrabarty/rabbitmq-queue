2ca3675e8a3809d5ba058a5240d4fd477e12086d
DiptoChakrabarty <diptochuck123@gmail.com>
2020-11-19 18:22:21 +0530

    new tfidf code added

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 
 - Run Rabbit MQ Command : sudo docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3-management
+
+
---
+++ tfdf.py
@@ -0,0 +1,27 @@
+
+import numpy as np
+import pandas as pd
+import re
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+
+
+
+def compute(filename):
+    sentences = list()
+    with open(filename) as file:
+        for line in file:
+            for l in re.split(r"\.\s|\?\s|\!\s|\n",line):
+                if l:
+                    sentences.append(l)
+    cov = CountVectorizer(stop_words='english', min_df=3, max_df=0.5, ngram_range=(1,2))
+    sf = cov.fit_transform(sentences)
+
+
+    transform = TfidfTransformer()
+    transform_weights = transform.fit_transform(sf)
+    weights = np.asarray(transformed_weights.mean(axis=0)).ravel().tolist()
+    weights_df = pd.DataFrame({'term': cov.get_feature_names(), 'weight': weights})
+
+    return weights_df.sort_values(by='weight', ascending=False).head(10)
+
